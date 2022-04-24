af16fd773c44d85593f7093598009144376e2f35
AnKoi <gtsk5dragon@gmail.com>
2022-04-24 09:57:46 +0700

    ok

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Omnifood</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <a href="#">
+            <img class="logo" src="images/logo.png" alt="">
+        </a>
+        <nav class="main-nav">
+            <ul class="main-nav-list">
+                <li>
+                    <a href="#" class="main-nav-link">How it works</a>
+                    <a href="#" class="main-nav-link">Meals</a>
+                    <a href="#" class="main-nav-link">Testimonials</a>
+                    <a href="#" class="main-nav-link">Pricing</a>
+                    <a href="#" class="main-nav-link">Try for free</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main">
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
